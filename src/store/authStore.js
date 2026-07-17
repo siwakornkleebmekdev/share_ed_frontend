01c96c760d06e8515081076eb2b15d362a9bf26a
Kittipong001 <xnokewro@gmail.com>
2026-07-17 14:58:04 +0700

    ADD Profile , Setting Profile , achievements/reward_claiming , Fixing AuthStore , Token access google , logout

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -4,7 +4,10 @@ const useAuthStore = create((set) => ({
   isAuthenticated: false,
   user: null,
   login: (userData) => set({ isAuthenticated: true, user: userData }),
-  logout: () => set({ isAuthenticated: false, user: null }),
+  logout: () => {
+    localStorage.removeItem('access_token');
+    set({ isAuthenticated: false, user: null });
+  },
 }));
 
 export default useAuthStore;
