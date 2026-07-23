1ef0bbd4abe749178d55cecfee4f180c4856d6e2
siwakorn <siwakornkleebmek.dev@gmail.com>
2026-07-24 02:10:06 +0700

    Fixed

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,29 +2,9 @@ import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
   isAuthenticated: false,
-  isInitializing: true,
   user: null,
-<<<<<<< HEAD
-  login: (userData) => set({ isAuthenticated: true, isInitializing: false, user: userData }),
-  logout: () => {
-    try {
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('login_timestamp');
-    } catch (e) {}
-    set({ isAuthenticated: false, isInitializing: false, user: null });
-  },
-  setInitializing: (isInitializing) => set({ isInitializing }),
-=======
   login: (userData) => set({ isAuthenticated: true, user: userData }),
-  logout: () => {
-    localStorage.removeItem('access_token');
-    set({ isAuthenticated: false, user: null });
-  },
-<<<<<<< HEAD
->>>>>>> 01c96c7 (ADD Profile , Setting Profile , achievements/reward_claiming , Fixing AuthStore , Token access google , logout)
-=======
->>>>>>> 2ded06a56b2e81e5aa0eedc83b3f6983fa0c15ee
+  logout: () => set({ isAuthenticated: false, user: null }),
 }));
 
 export default useAuthStore;
-
